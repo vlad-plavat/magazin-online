8f612a9831f292d6c52dbdf1a2dd4218529e71db
roxi <roxana.podean@student.upt.ro>
2022-05-09 12:52:37 +0300

    NL-43 Popup feedback

--- src/main/java/org/nl/controllers/FeedbackController.java
+++ src/main/java/org/nl/controllers/FeedbackController.java
@@ -7,7 +7,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.nl.Main;
 import org.nl.services.FeedbackService;
@@ -15,6 +17,7 @@ import org.nl.services.UserService;
 
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Date;
 
 import static org.nl.controllers.RegistrationController.loggeduser;
@@ -47,6 +50,26 @@ public class FeedbackController {
 
         FeedbackService.addFeedback(loggeduser.getUsername(), text, new Date());
         //FeedbackService.printAllFeedback();
+        try {
+            URL toFxml = Main.class.getClassLoader().getResource("PopupFeedback.fxml");
+            if (toFxml == null)
+                throw new RuntimeException("Could not load PopupFeedback.fxml");
+            Parent root = FXMLLoader.load(toFxml);
+
+
+            final Stage dialog = new Stage();
+            dialog.initModality(Modality.APPLICATION_MODAL);
+            dialog.setTitle("Feedback sent");
+            dialog.setResizable(false);
+            dialog.initOwner(((Node) evt.getSource()).getScene().getWindow());
+            dialog.getIcons().add(new Image("icon.png"));
+            Scene scene = new Scene(root);
+            dialog.setScene(scene);
+            dialog.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
         backToMenu(evt);
     }
 
